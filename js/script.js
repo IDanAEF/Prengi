8fbff67f79dd0f4303d79a0877a7b5240e80728d
Denis <efremov89deniss55@gmail.com>
2021-06-29 15:13:48 +0500

    Done desktop version

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.solution__slider').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/arr_prev.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/arr_next.png"></button>',
+    infinite: true,
+    arrows: true
+  });
+});
